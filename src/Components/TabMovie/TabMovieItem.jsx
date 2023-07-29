843aac8ddf98053f1655706f4f2af2af1fa10751
AcedenD <adenddang@gmail.com>
2023-07-29 22:09:01 +1000

    fixed css movietab

--- src/Components/TabMovie/TabMovieItem.jsx
+++ src/Components/TabMovie/TabMovieItem.jsx
@@ -36,11 +36,11 @@ const TabMovieItem = ({ maHeThongRap }) => {
               if (item.dangChieu) {
                 return (
                   <div className="flex" key={index}>
-                    <div className="w-2/12">
+                    <div className="w-1/4 ">
                       <img src={item.hinhAnh} alt="" />
                     </div>
                     <div className="w-10/12 ml-3">
-                      <h3 className="text-2xl font-bold">{item.tenPhim}</h3>
+                      <h3 className="text-xl font-bold">{item.tenPhim}</h3>
                       <div className="flex flex-wrap">
                         {item.lstLichChieuTheoPhim
                           .slice(0, 5)
@@ -48,7 +48,7 @@ const TabMovieItem = ({ maHeThongRap }) => {
                             return (
                               <p
                                 key={index}
-                                className="w-1/3 border border-gray-300 rounded-md py-2 px-3 my-2 shadow-md mr-2"
+                                className="w-1/3 border border-gray-300 rounded-md py-2 px-3 my-2 shadow-md mr-2 min-w-max"
                               >
                                 <span className="mr-2 font-bold text-green-600">
                                   {moment(suatChieu.ngayChieuGioChieu).format(
