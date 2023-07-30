7761c9823d8e96733bdd0c1c1ec53439d8a1015e
My Quach <mkq5472@gmail.com>
2023-07-30 12:17:57 +0700

    commit

--- src/Components/FormAddLich/FormAddLich.jsx
+++ src/Components/FormAddLich/FormAddLich.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { rapServ } from './../../services/rapServices';
 import { movieServ } from '../../services/movieServices';
-import { Formik, Form, Field } from 'formik';
+
 
 const FormAddLich = () => {
-  
 
-  //Table
+  //tittle & Pic
   const [moviePhim, setMoviePhim] = useState([]);
 const location = useLocation();
       const searchParams = new URLSearchParams(location.search);
@@ -27,9 +26,22 @@ console.log (maPhim)
       }, []);
 
   //selections
+
+  const handleHeThongRapChange = (event) => {
+    setSelectedHeThongRap(event.target.options[event.target.selectedIndex].id);
+  };
+  // const handleCumRapChange = (event) => {
+  //   setSelectedCumRap(event.target.options[event.target.selectedIndex].id);
+  // };
+  
+
   const [heThongRap, setHeThongRap] = useState([]);
   const [selectedHeThongRap, setSelectedHeThongRap] = useState('');
   const [cumRap, setCumRap] = useState([]);
+  const [selectedDanhSachRap, setSelectedDanhSachRap] = useState([]);
+  const [tenRap, setTenRap] = useState([]);
+  const [selectedCumRap, setSelectedCumRap] = useState("");
+
 
   useEffect(() => {
     rapServ
@@ -57,10 +69,23 @@ console.log (maPhim)
     }
   }, [selectedHeThongRap]);
 
-  const handleHeThongRapChange = (event) => {
-    setSelectedHeThongRap(event.target.options[event.target.selectedIndex].id);
+  useEffect(() => {
+    const selectedCumRapInfo = cumRap.find((cumRap) => cumRap.tenCumRap === selectedCumRap);
+    if (selectedCumRapInfo) {
+      setSelectedDanhSachRap(selectedCumRapInfo.danhSachRap);
+      const tenRapList = selectedCumRapInfo.danhSachRap.map((rap) => rap.tenRap);
+      setTenRap(tenRapList);
+    } else {
+      setSelectedDanhSachRap([]);
+      setTenRap([]);
+    }
+  }, [selectedCumRap, cumRap]);
+
+  const handleCumRapChange = (event) => {
+    setSelectedCumRap(event.target.value);
   };
 
+//render options
   const renderHeThong = () => {
     return heThongRap.map((item) => (
       <option key={item.maHeThongRap} value={item.tenHeThongRap} id={item.maHeThongRap}>
@@ -76,16 +101,65 @@ console.log (maPhim)
       </option>
     ));
   };
+ 
+  const renderTenRap = () => {
+    return tenRap.map((item, index) => (
+      <option key={index} value={item.maRap}>
+        {item}
+      </option>
+    ));
+  };
+
+    //functionAddLichChieu
+    const [ngayChieuGioChieu, setNgayChieuGioChieu] = useState("");
+    const [selectedRap, setSelectedRap] = useState("");
+    const [giaVe, setGiaVe] = useState("");
+
+      const handleNgayChieuGioChieuChange = (event) => {
+        setNgayChieuGioChieu(event.target.value);
+      };
+    
+      const handleRapChange = (event) => {
+        setSelectedRap(event.target.value);
+      };
+    
+      const handleGiaVeChange = (event) => {
+        setGiaVe(event.target.value);
+    };
+    // const formData = {
+    //   maPhim: maPhim,
+    //   ngayChieuGioChieu: values.ngayChieuGioChieu,
+    //   maRap: values.cumRap, 
+    //   giaVe: values.giaVe,
+    // };
+    const addLichChieu = async () => {
+      try {
+        const formData = {
+          maPhim: maPhim,
+          ngayChieuGioChieu: ngayChieuGioChieu,
+          maRap: selectedRap.value,
+          giaVe: giaVe,
+        };
+  
+        const response = await movieServ.addLichChieu(formData);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+
 
   return (
   <div>
+    
     <div className="flex flex-col items-center space-y-4 mb-5">
     <h1 className="text-2xl font-bold text-gray-900 dark:text-white">{moviePhim.tenPhim}</h1>
           <img src={moviePhim.hinhAnh}
             alt="Movie Poster"
             className="w-60 h-auto "/>
       </div>
-    <form className='border rounded p-5'>
+    <form  className='border rounded p-5' onSubmit={addLichChieu}>
         <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
         <div className="w-full"> 
       <label htmlFor="heThongRap" className="me-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
@@ -100,16 +174,25 @@ console.log (maPhim)
       <label htmlFor="cumRap" className="me-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
         Cụm Rạp
       </label>
-      <select>
+      <select onChange={handleCumRapChange}>
         {renderCumRap()}
       </select>
       </div>
 
       <div className="w-full">
+  <label htmlFor="tenRap" className="me-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+    Rạp Chiếu
+  </label>
+  <select onChange={handleRapChange}>
+    {renderTenRap()}
+  </select>
+</div>
+
+      <div className="w-full">
       <label htmlFor="ngayChieuGioChieu"  className="me-3 block mb-2 text-sm font-medium text-gray-900 dark:text-white">
         Ngày Chiếu, Giờ Chiếu
       </label>
-      <input type="datetime-local" id="ngayChieuGioChieu"  />
+      <input type="datetime-local" id="ngayChieuGioChieu" onChange={handleNgayChieuGioChieuChange}  />
 </div>
 
 <div className="w-full">
@@ -118,6 +201,7 @@ console.log (maPhim)
       </label>
       <input
       placeholder='Nhập giá vé'
+      onChange={handleGiaVeChange}
         type="number"
         id="giaVe"
         min="0"
@@ -130,6 +214,7 @@ console.log (maPhim)
       </button>
       </div>
     </form>
+    {/* <p>maphim{maPhim}ngay{ngayChieuGioChieu}cumrap{selectedRap}gia{giaVe}</p> */}
 </div>
 
   );
